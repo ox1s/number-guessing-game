806413336bd3635295e997aff023a0d10cba4641
Palina Mialyantsava <linamel08v@gmail.com>
2025-04-13 19:50:00 +0000

    fix: fix insert results

--- number_guess.sh
+++ number_guess.sh
@@ -33,7 +33,7 @@ RANDOM_NUMBER=$((1 + RANDOM % 1000))
 TRIES=0
 GUES=0
 
-echo -e "\nGuess the secret number between 1 and 1000:"
+echo "Guess the secret number between 1 and 1000:"
 while [[ $( echo $GUESS | sed 's/ //g') -ne $RANDOM_NUMBER ]]
 do
   read GUESS
@@ -42,15 +42,15 @@ do
     TRIES=$((TRIES + 1))
     if [[ $GUESS -gt $RANDOM_NUMBER ]]
       then
-      echo -e "\nIt's higher than that, guess again:"
+      echo "It's higher than that, guess again:"
     elif [[ $GUESS -lt $RANDOM_NUMBER ]]
       then
-      echo -e "\nIt's lower than that, guess again:"
+      echo "It's lower than that, guess again:"
     fi
   else
-  echo -e "\nThat is not an integer, guess again:"
+    echo "That is not an integer, guess again:"
   fi
 done
 
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(number_of_guesses,user_id) VALUES($TRIES, $USER_ID)")
-echo -e "\nYou guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+echo "You guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
