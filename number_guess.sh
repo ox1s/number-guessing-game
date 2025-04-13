a512fa14f631f7845efef24be28eff701b639373
Palina Mialyantsava <linamel08v@gmail.com>
2025-04-13 18:36:38 +0000

    feat: basic logic of game

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,21 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 echo -e "Enter your username:"
 read USERNAME
+
+while [[ -z $USERNAME ]] 
+  do
+  read USERNAME
+done
+#trim input
+USERNAME=$(echo $USERNAME | sed 's/ //g')
+#get user_id
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE name=$USERNAME")
+#if don't exist
+if [[ -z $USER_ID]]
+  then
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+else
+  GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE user_id=$USER_ID")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
