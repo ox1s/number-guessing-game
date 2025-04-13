6baeb72c91f390bf3ea39a478e8ab4bafaa61561
Palina Mialyantsava <linamel08v@gmail.com>
2025-04-13 20:10:41 +0000

    fix: now it's work ok

--- number_guess.sh
+++ number_guess.sh
@@ -24,7 +24,7 @@ if [[ -z $USER_ID ]]
   # get new user_id
   USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")
 else
-  USERNAME=$($PSQL "SELECT name FROM users WHERE name='$USERNAME'")
+  #USERNAME=$($PSQL "SELECT name FROM users WHERE name='$USERNAME'")
   GAMES_PLAYED=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
   BEST_GAME=$($PSQL "SELECT MIN(number_of_guesses) FROM games WHERE user_id=$USER_ID")
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
@@ -32,25 +32,29 @@ fi
 
 RANDOM_NUMBER=$((1 + RANDOM % 1000))
 TRIES=0
-GUES=0
 
 echo "Guess the secret number between 1 and 1000:"
-while [[ $( echo $GUESS | sed 's/ //g') -ne $RANDOM_NUMBER ]]
+
+# game loop
+while true 
 do
-  read GUESS
-  if echo $GUESS | grep -qE '^[0-9]+$' 
+read GUESS
+
+if echo "$GUESS" | grep -qE '^[0-9]+$'
+  then
+  ((TRIES++))
+  if [[ $GUESS -eq $RANDOM_NUMBER ]]
+  then
+    break
+  elif [[ $GUESS -gt $RANDOM_NUMBER ]]
     then
-    TRIES=$((TRIES + 1))
-    if [[ $GUESS -gt $RANDOM_NUMBER ]]
-      then
-      echo "It's higher than that, guess again:"
-    elif [[ $GUESS -lt $RANDOM_NUMBER ]]
-      then
-      echo "It's lower than that, guess again:"
-    fi
-  else
-    echo "That is not an integer, guess again:"
+    echo "It's higher than that, guess again:"
+  else 
+    echo "It's lower than that, guess again:"
   fi
+else
+  echo "That is not an integer, guess again:"
+fi
 done
 
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(number_of_guesses,user_id) VALUES($TRIES, $USER_ID)")
