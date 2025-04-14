cf6d853825cebdb1a3efe77949d290ef395a3b59
Palina Mialyantsava <linamel08v@gmail.com>
2025-04-14 05:53:16 +0000

    test: add \n everywheere

--- number_guess.sh
+++ number_guess.sh
@@ -3,7 +3,7 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 
-echo "Enter your username:"
+echo -e "\nEnter your username:"
 read USERNAME
 
 # trim input
@@ -15,7 +15,7 @@ USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$USERNAME'")
 # if don't exist
 if [[ -z $USER_ID ]]
   then
-  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
   # add user
   INSERT=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
   # get new user_id
@@ -37,7 +37,7 @@ fi
 RANDOM_NUMBER=$((1 + RANDOM % 1000))
 TRIES=0
 
-echo "Guess the secret number between 1 and 1000:"
+echo -e "\nGuess the secret number between 1 and 1000:\n"
 
 # game loop
 while true 
@@ -52,13 +52,13 @@ if echo "$GUESS" | grep -qE '^[0-9]+$'
     break
   elif [[ $GUESS -lt $RANDOM_NUMBER ]]
     then
-    echo "It's higher than that, guess again:"
+    echo -e "\nIt's higher than that, guess again:\n"
   else 
-    echo "It's lower than that, guess again:"
+    echo -e "\nIt's lower than that, guess again:\n"
   fi
 else
-  echo "That is not an integer, guess again:"
+  echo -e "\nThat is not an integer, guess again:\n"
 fi
 done
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(number_of_guesses,user_id) VALUES($TRIES, $USER_ID)")
-echo "You guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+echo -e "\nYou guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
